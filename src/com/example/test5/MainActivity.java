48f2ed54668694f34c708f3c0c6a6896f8ca7570
Denis Repp <reppden@gmail.com>
2013-05-25 16:12:58 +0600

    something is works!!! omg!!!

--- src/com/example/test5/MainActivity.java
+++ src/com/example/test5/MainActivity.java
@@ -1,19 +1,19 @@
 package com.example.test5;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.*;
-import com.example.test5.map.MapActivity;
 import com.example.test5.models.TaskModel;
 import com.example.test5.create_task.CreateTaskActivity;
-import com.example.test5.view_task.ViewTaskActivity;
 
 public class MainActivity extends Activity {
 
-    Button quickAdd, mapBtn;
+    ImageButton addBtn, mapBtn, quickAddButton;
     ListView taskList;
     EditText taskText;
     ArrayAdapter<TaskModel> adapter;
@@ -25,8 +25,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
         taskText = (EditText) findViewById(R.id.main_taskText);
         taskList = (ListView) findViewById(R.id.main_tasklist);
-        quickAdd = (Button) findViewById(R.id.main_addbutton);
-        mapBtn = (Button) findViewById(R.id.main_mapBtn);
+        addBtn = (ImageButton) findViewById(R.id.main_addbutton);
+        mapBtn = (ImageButton) findViewById(R.id.main_mapBtn);
+        quickAddButton = (ImageButton) findViewById(R.id.main_quickAddButton);
 
         // неведомая фигня инициализирующая список
         adapter = new ArrayAdapter<TaskModel>(this.getBaseContext(), R.layout.list_view,R.id.list_view_taskText);
@@ -35,7 +36,15 @@ public class MainActivity extends Activity {
     }
 
     private void initListeners() {
-        quickAdd.setOnClickListener(new View.OnClickListener() {
+        quickAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (taskText.getText() != null && !taskText.getText().toString().isEmpty()) {
+                    addItemToTaskList(taskText.getText().toString());
+                }
+            }
+        });
+        addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CreateTaskActivity.class);
@@ -48,16 +57,7 @@ public class MainActivity extends Activity {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (event.getAction() == KeyEvent.ACTION_DOWN) {
                     if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                        String text = ((EditText) v).getText().toString();
-                        if (!text.trim().isEmpty()) {
-                            //create a simple task
-                            // TODO (Drepp) запихать куда нибудь свежесозданную таску
-                            TaskModel task = new TaskModel(0L, text);
-
-                            // add a task into list
-                            adapter.insert(task, 0);
-                            ((EditText) v).setText("");
-                        }
+                        addItemToTaskList(((EditText) v).getText().toString());
                     }
                 }
                 return true;
@@ -66,18 +66,50 @@ public class MainActivity extends Activity {
         taskList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, ViewTaskActivity.class);
-                intent.putExtra(getString(R.string.viewTask_TaskModel), adapter.getItem(position));
-                MainActivity.this.startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this, ViewTaskActivity.class);
+//                intent.putExtra(getString(R.string.viewTask_TaskModel), adapter.getItem(position));
+//                MainActivity.this.startActivity(intent);
+                reliseOmNomNom();
             }
         });
 
         mapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MapActivity.class);
-                MainActivity.this.startActivity(intent);
+                // TODO вызвать карту
+                reliseOmNomNom();
             }
         });
+    }
+
+    private void addItemToTaskList(String text) {
+        if (!text.trim().isEmpty()) {
+            //create a simple task
+            // TODO (Drepp) запихать куда нибудь свежесозданную таску
+            TaskModel task = new TaskModel(0L, text);
+
+            // add a task into list
+            adapter.insert(task, 0);
+            taskText.setText("");
+        }
+    }
+
+    private void reliseOmNomNom() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Are u sure about it?")
+                .setPositiveButton("NoNoNoNo!!!", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton("NomNomNomNom!!", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        builder.create();
+        builder.show();
+
     }
 }
